9650227ceae50560e6ca123a345a8bf9c19d06cd
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-10-09 13:18:03 +0100

    Add files via upload

---
+++ hello world 1.py
@@ -0,0 +1,14 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("hello world")
+hello world
+>>> 
+print ("I am in my ISD class right now")
+I am in my ISD class right now
+>>> name = input ("what is your name? ")
+what is your name? 
+>>> print ("Hello " + name)
+Hello 
+>>> print ("How are you")
+How are you
+>>> 
