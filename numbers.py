50b8556c977c7794415151a6c33decb6d6ac4b82
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-10-14 16:06:32 +0100

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,14 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+# Store input numbers
+>>> num1 = input('Enter first number: ')
+Enter first number: 1
+>>> num2 = input('Enter second number: ')
+Enter second number: 2
+>>> # Add two numbers
+>>> sum = float(num1) + float(num2)
+>>> #Display the sum
+>>> print('The sum of {0} and {1} is {2}' .format(num1, num2, sum))
+The sum of 1 and 2 is 3.0
+>>> 
