3fac82455334ab2cb40c34f9ca1c1aac0307eef1
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-10-14 18:29:35 +0100

    Add files via upload

---
+++ output code.py
@@ -0,0 +1,10 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+num = 4
+>>> num*=2
+>>> num1=num+2
+>>> num1+=3
+>>> print(num1)
+13
+>>> 
