cb6291e084d791c702be670b796fad7c618d6eba
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-10-23 13:45:56 +0100

    Add files via upload

---
+++ radius missing float.py
@@ -0,0 +1,9 @@
+radius = int(input("Radius:"))
+
+x = 3.14
+
+pi = x
+
+area = pi * radius ** 2
+
+print(area)
---
+++ radius missing solved.py
@@ -0,0 +1,12 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: E:/radius missing float.py ====================
+hello
+Radius:67
+>>> 
+==================== RESTART: E:/radius missing float.py ====================
+hello
+Radius:877
+2415065.06
+>>> 
