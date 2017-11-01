0cf3427f6b7d167f9395af8eb6dba69dc986f3b7
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-11-01 21:55:08 +0000

    Add files via upload

---
+++ ps4 q9 string format.py
@@ -0,0 +1,10 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> radius = 2
+>>> area = math.pi*(radius**2)
+>>> print("the area of a circle is:\t%.2f"%area)
+the area of a circle is:	12.57
+>>> print("the radius is:\t%.2f"%radius)
+the radius is:	2.00
+>>> 
