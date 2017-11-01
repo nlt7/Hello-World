d710023ec879bda51b750a0a2522d3101e7a6638
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-11-01 14:44:53 +0000

    Add files via upload

---
+++ ps4 q4 - missing brackets.py
@@ -0,0 +1,13 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print(3+4+5/3)
+8.666666666666666
+>>> print(3+4+5)
+12
+>>> print(12/3)
+4.0
+>>> print((3+4+4)/3)
+3.6666666666666665
+>>> print((3+4+5)/3)
+4.0
+>>> 
