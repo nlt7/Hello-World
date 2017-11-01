7fd6e77c832ed8de8e0981328a66e37709206738
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-11-01 20:20:40 +0000

    Add files via upload

---
+++ ps4 q8 statement.py
@@ -0,0 +1,13 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> input("Enter your name: ")
+Enter your name: Alex
+'Alex'
+>>> age = input("Enter yout age: ")
+Enter yout age: 22
+>>> int(age)
+22
+>>> age = int(age)
+>>> print("Hello Alex, next yeat you will be",age+1,"years old")
+Hello Alex, next yeat you will be 23 years old
+>>> 
