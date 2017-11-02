21752b892da42bf03bf8a7aeb2872150e1360735
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-11-02 00:20:01 +0000

    Add files via upload

---
+++ ps4 q10 values.py
@@ -0,0 +1,13 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> p=17
+>>> q=18
+>>> p//10+p%10
+8
+>>> p%2+q%2
+1
+>>> (p+q)//2
+17
+>>> (p+q)/2.0
+17.5
+>>> 
