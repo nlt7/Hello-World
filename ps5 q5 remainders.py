1a56a1b7845ce0ce993e40f14c6981681801bb24
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-11-03 16:59:55 +0000

    Add files via upload

---
+++ ps5 q5 remainders.py
@@ -0,0 +1,8 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> input("Please enter a whole number")
+Please enter a whole number 12
+' 12'
+>>> print(12%7)
+5
+>>> 
