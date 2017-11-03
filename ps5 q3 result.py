55dbbaf3573e1cb9ab7f549ad7af98d801f5abaf
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-11-03 12:53:40 +0000

    Add files via upload

---
+++ ps5 q3 result.py
@@ -0,0 +1,9 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> num1=int(input("Enter first number:"))
+Enter first number: 12
+>>> num2=int(input("Enter second number:"))
+Enter second number: 14
+>>> result=int(num1+num2)
+>>> print("The result is"+result)
+
