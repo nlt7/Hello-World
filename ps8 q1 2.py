de73cd529a09d19d0b7888fe45424ee8342728c8
nlt7 <32064423+nlt7@users.noreply.github.com>
2017-11-15 15:45:07 +0000

    Add files via upload

---
+++ ps8 q1 2.py
@@ -0,0 +1,3 @@
+def mystery(x=3,y=2):
+    result=(x+y)/(y-x)
+    return result
---
+++ ps8 q2 2.py
@@ -0,0 +1,6 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======================== RESTART: K:/Python/ps8 q2.py ========================
+6.0
+>>> 
